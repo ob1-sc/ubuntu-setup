6a9f5700099072b406ee168ca9c96d1db6f15938
Simon O'Brien <simon@the-obriens.net>
2017-09-30 15:01:27 +0100

    updated setup script to support snap and deb installs

--- setup.sh
+++ setup.sh
@@ -1,36 +1,87 @@
 #!/bin/sh
 
+repeat_char() {
+	
+	local CHAR=$1
+	local LEN=$2
+	
+	printf '%*s' "$LEN" | tr ' ' "$CHAR"
+}
+
 print_header() {
 
-	HEADER_TEXT="======= $1 ======="
+	local HEADER_TEXT="======= $1 ======="
 
 	echo $HEADER_TEXT | tr "[:print:]" =
 	echo $HEADER_TEXT
 	echo $HEADER_TEXT | tr "[:print:]" =
 
-	exit
+	HEADER_TEXT_LENGTH=${#HEADER_TEXT}
 }
 
-print_header "updating apt cache"
-sudo apt-get update
+print_footer() {
 
-# gdebi - simple tool to install deb files.
-# It lets you install local deb packages resolving and installing its dependencies.
-sudo apt-get install gdebi -y
+	repeat_char "=" $1
+}
+
+run_cmd() {
+
+	local CMD="$1"
+	local CMD_DESC="$2"
+
+	HEADER_TEXT_LENGTH=0
+
+	print_header "$CMD_DESC"
+	
+	eval $CMD
+
+	print_footer $HEADER_TEXT_LENGTH
+	printf "\n\n"
+
+	HEADER_TEXT_LENGTH=0
+}
+
+install_deb() {
+
+	local DEB_URL="$1"
+	local DESC="$2"
+	local FILE_NAME=package.deb
+
+	local CMD="wget $DEB_URL -O $FILE_NAME && sudo gdebi $FILE_NAME -n && rm -f -v $FILE_NAME"
+
+	run_cmd "$CMD" "$DESC"
+}
+
+install_snap() {
+
+	local SNAP_PACKAGE="$1"
+	local DESC="$2"
+
+	local CMD="sudo snap install $SNAP_PACKAGE"
+	
+	run_cmd "$CMD" "$DESC"
+}
+
+# ensure the apt cache is up to date
+run_cmd "sudo apt-get update" "updating apt cache"
+
+# install gdebi - simple tool to install deb files.
+run_cmd "sudo apt-get install gdebi -y" "installing gdebi"
+
+# install tbux & byobu (great terminal combination)
+run_cmd "sudo apt-get install tmux byobu" "installing byobu & tmux"
+
+# install google chrome
+install_deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" "installing google chrome"
+
+# install slack
+install_deb "https://downloads.slack-edge.com/linux_releases/slack-desktop-2.8.0-amd64.deb" "installing slack"
 
 # simplenote 
-sudo snap install simplenote
+install_snap "simplenote" "installing simplenote"
 
-# tbux & byobu (great terminal combination)
-sudo apt-get install tmux byobu -y
+exit
 
-# google chrome
-
-# slack
-FILE_SLACK=slack-desktop-2.8.0-amd64.deb
-wget https://downloads.slack-edge.com/linux_releases/$FILE_SLACK
-sudo gdebi $FILE_SLACK -n
-rm -f -v $FILE_SLACK
 
 # Oracle JDK8 (jdk&jre)
 
