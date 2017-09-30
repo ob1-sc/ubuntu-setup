da05dd1b88d13b46f894f72af34913f1a678e363
Simon O'Brien <simon@the-obriens.net>
2017-09-30 19:09:28 +0100

    added support for install apt packages with optional repositories

--- setup.sh
+++ setup.sh
@@ -41,49 +41,76 @@ run_cmd() {
 	HEADER_TEXT_LENGTH=0
 }
 
-install_deb() {
+install_deb_pkg() {
 
-	local DEB_URL="$1"
+	local DEB_PKG_URL="$1"
 	local DESC="$2"
 	local FILE_NAME=package.deb
 
-	local CMD="wget $DEB_URL -O $FILE_NAME && sudo gdebi $FILE_NAME -n && rm -f -v $FILE_NAME"
+	local CMD="wget $DEB_PKG_URL -O $FILE_NAME && sudo gdebi $FILE_NAME -n && rm -f -v $FILE_NAME"
 
 	run_cmd "$CMD" "$DESC"
 }
 
-install_snap() {
+install_snap_pkg() {
 
-	local SNAP_PACKAGE="$1"
+	local SNAP_PKG="$1"
 	local DESC="$2"
 
-	local CMD="sudo snap install $SNAP_PACKAGE"
+	local CMD="sudo snap install $SNAP_PKG"
 	
+	run_cmd "$CMD" "$DESC"
+}
+
+install_apt_pkg() {
+
+	local APT_PKG="$1"
+	local DESC="$2"
+	local APT_REPO="$3"
+
+	local CMD="sudo apt-get install $APT_PKG -y"
+
+	if [ ! -z $APT_REPO ]; then
+		CMD="sudo add-apt-repository $APT_REPO -y -u && $CMD"
+	fi
+
 	run_cmd "$CMD" "$DESC"
 }
 
 # ensure the apt cache is up to date
 run_cmd "sudo apt-get update" "updating apt cache"
 
+# Oracle JDK8 (jdk&jre)
+install_apt_pkg "oracle-java8-installer oracle-java8-set-default" "installing Oracle Java 8" "ppa:webupd8team/java"
+
 # install gdebi - simple tool to install deb files.
-run_cmd "sudo apt-get install gdebi -y" "installing gdebi"
+install_apt_pkg "gdebi" "installing gdebi"
 
 # install tbux & byobu (great terminal combination)
-run_cmd "sudo apt-get install tmux byobu" "installing byobu & tmux"
+install_apt_pkg "tmux byobu" "installing byobu & tmux"
 
 # install google chrome
-install_deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" "installing google chrome"
+install_deb_pkg "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" "installing google chrome"
 
 # install slack
-install_deb "https://downloads.slack-edge.com/linux_releases/slack-desktop-2.8.0-amd64.deb" "installing slack"
+install_deb_pkg "https://downloads.slack-edge.com/linux_releases/slack-desktop-2.8.0-amd64.deb" "installing slack"
 
 # simplenote 
-install_snap "simplenote" "installing simplenote"
+install_snap_pkg "simplenote" "installing simplenote"
+
+# Remmina
+install_apt_pkg "remmina remmina-plugin-rdp libfreerdp-plugins-standard" "install remmina" "ppa:remmina-ppa-team/remmina-next"
+
+# Remarkable
+install_deb_pkg "http://remarkableapp.github.io/files/remarkable_1.87_all.deb" "installing remarkable"
+
+# ensure packages are up to date
+run_cmd "sudo apt-get update && sudo apt-get upgrade -y" "ensuring all packages are up to date"
+
+# clean up
+run_cmd "sudo apt-get autoremove" "cleaning up"
 
 exit
-
-
-# Oracle JDK8 (jdk&jre)
 
 # Postman
 
@@ -92,7 +119,3 @@ exit
 # Docker CE
 
 # Docker Compose
-
-# Remmina
-
-# Remarkable
